870ac8c8094ba18f1173ed33c5e336c7c6afa952
ductai98 <ronin32014@gmail.com>
2020-07-06 03:14:22 +0700

    Update join room

--- socket/app.js
+++ socket/app.js
@@ -6,8 +6,8 @@ var io = require("socket.io")(server);
 server.listen(port, () => console.log("Server running in port " + port));
 
 let data = [];
-var room = [];
-
+var roomId = 0;
+var roomList = [];
 io.on("connection", function (socket) {
     const dataServer = data;
     console.log(socket.id + ": connected");
@@ -33,16 +33,46 @@ io.on("connection", function (socket) {
 
     //Tạo room khi có sự kiện tạo
     socket.on("createRoom", (data) => {
-       console.log(data); 
-       room.push(data);
-       socket.join(data);
-       io.sockets.emit("sendRoomList", { room });
+        console.log(`on getRoomById`);
+        roomId++;
+        data.id = roomId;
+        console.log(data); 
+        roomList.push(data);
+        socket.join(roomId);
+        console.log(`emit sendRoomToOwner`);
+        io.to(socket.id).emit('sendRoom', data);
     });
 
     //Trả về room khi không có sự kiện nào gọi đến
     socket.on("getRoom", () => {
-        io.sockets.emit("roomList", {room});
+        io.sockets.emit("roomList", {roomList});
     });
+
+
+    socket.on('getRoomById', roomId =>{
+        console.log(`on getRoomById`);
+        
+        const id = roomId;
+        const room = roomList.find(item=>item.id===id);
+        console.log(`emit send Room for info`);
+        io.to(socket.id).emit('sendRoomInfo', room);
+    });
+
+    socket.on("joinRoom", (data) => {
+        console.log('on joinRoom: ');
+        console.log(data);
+        socket.join(data.id);
+        roomList.forEach(item=>{
+            if(item.id == data.roomId){
+                item.players.push(data.playerName);
+                console.log('on sendRoom');
+                io.in(data.roomId).emit("sendRoom", item);
+                return;
+            }
+        });
+    });
+
+    
     
 });
 
