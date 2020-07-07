a44c25ee7c0edc8a11f2c9b1a09d11f9255c2837
ductai98 <ronin32014@gmail.com>
2020-07-08 00:34:19 +0700

    Play game part 1

--- socket/app.js
+++ socket/app.js
@@ -10,6 +10,34 @@ let data = [];
 var roomId = 0;
 var roomList = [];
 var gameList = [];
+
+var Room = function(id, name, numOfPlayers, password, time, players, owner){
+    this.id = id;
+    this.name = name;
+    this.numOfPlayers = numOfPlayers;
+    this.password = password;
+    this.time = time;
+    this.players = players;
+    this.owner = owner;
+
+    var interval;
+    this.startTimer = () => {
+        var tempTime = this.time;
+        interval = setInterval(() => {
+            console.log(tempTime);
+            if(tempTime === 0){
+                clearInterval(interval);
+            }
+            io.in(this.id).emit("sendTimer", tempTime);
+            tempTime = tempTime - 1;
+        }, 1000);
+    }
+
+    this.stopTimer = () =>{
+        clearInterval(interval);
+    }
+}
+
 io.on("connection", function (socket) {
     const dataServer = data;
     console.log(socket.id + ": connected");
@@ -37,11 +65,12 @@ io.on("connection", function (socket) {
     socket.on("createRoom", (data) => {
         console.log(`on create room : ${JSON.stringify(data)}`);
         roomId++;
-        data.id = roomId;
-        roomList.push(data);
+        var newRoom = new Room(roomId, data.name, data.numOfPlayers, data.password, data.time, data.players, data.owner);
+        console.log(newRoom);
+        roomList.push(newRoom);
         socket.join(roomId);
         console.log(`emit sendRoomToOwner: ` + JSON.stringify(data));
-        io.to(socket.id).emit('sendRoomOwner', data);
+        io.to(socket.id).emit('sendRoomOwner', newRoom);
     });
 
     //Trả về room khi không có sự kiện nào gọi đến
@@ -65,7 +94,7 @@ io.on("connection", function (socket) {
         socket.join(data.roomId);
         roomList.forEach(item=>{
             if(item.id == data.roomId){
-                item.players.push(data.playerName);
+                item.players.push(data.player);
                 console.log('emit send room info: ' + JSON.stringify(item));
                 io.in(data.roomId).emit("sendRoomInfo", item);
                 return;
@@ -78,37 +107,56 @@ io.on("connection", function (socket) {
         console.log(`data: ${roomId}`);
         
         const room = roomList.find(item => item.id === roomId);
+        console.log(room);
         if(room != null){
             let playerOrder = room.players;
-            let timer = room.timer;
             const ret = await wordModel.getRandomWord();
+            console.log(`currentWord: ${ret.Word}`);
+            
             const entity = {
                 id: roomId,
                 playerOrder,
                 currentWord: ret.Word,
             }
-
+            
+            gameList.push(entity);
             console.log(`emit send game`);
             io.in(roomId).emit("sendGame", entity);
-
             console.log(`emit timer`);
-            var timerInterval = setInterval(() => {
-                console.log(timer);
-                if(timer === 0){
-                    clearInterval(timerInterval);
-                }
-                io.in(roomId).emit("sendTimer", timer);
-                timer = timer - 1;
-            }, 1000);
+        
+            room.startTimer();
+
         }else{
             console.log(`room null`);
-            
         }
     });
 
-    socket.on('sendWord', data => {
+    socket.on('sendWord',async data => {
+        var room = roomList.find(item=>item.id === data.id);
+        room.stopTimer();
         console.log('on sendWord');
         console.log(data);
+        const gameId = data.id;
+        const game = gameList.find(item=>item.id === gameId);
+        if(game != null){
+            if(wordModel.isWordValid(data.word, game.currentWord)){
+                game.playerOrder.push(game.playerOrder.shift());
+                game.currentWord = data.word;
+                game.eliminatedPlayer = 0;
+                io.in(gameId).emit('sendGame', game);
+                room.startTimer();
+            }else{
+                const eliminatedPlayer = game.playerOrder[0].playerId;
+                game.playerOrder.shift();
+                game.eliminatedPlayer = eliminatedPlayer;
+                io.in(gameId).emit('sendGame', game);
+                if(game.playerOrder.length !== 1){
+                    room.startTimer();
+                }
+            }
+        }else{
+            console.log(`game null`);
+        }
     });
     
 });
--- socket/models/word.model.js
+++ socket/models/word.model.js
@@ -2,9 +2,24 @@ const db = require('../utils/db');
 
 module.exports = {
     getRandomWord: async () => {
-        const words = await db.load(`SELECT Word FROM av  
-                                ORDER BY RAND ( )  
-                                LIMIT 1  `);
+        const id = Math.floor(Math.random() * 40000) + 1;
+        console.log(`random id: ${id}`);
+        
+        const words = await db.load(`SELECT av.Word FROM av 
+                                        WHERE av.Id = ${id}`);
         return words[0];
+    },
+
+    isWordValid: (word, preWord) => {
+        const lastChar = preWord.slice(-1);
+        console.log(`${preWord}: ${lastChar}`);
+        const firstChar = word.charAt(0);
+        console.log(`${word}: ${firstChar}`);
+        
+        if(lastChar === firstChar){
+            console.log(`true`);
+            return true;
+        }
+        return false;
     }
 }
